e8b175d8fdb83eb1ddce66a7476d4a42a6577edd
charan <acharan.xyz@gmail.com>
2020-10-03 22:31:55 +0530

    deleted and added

--- leetcode solutions/rough work/exm.py
+++
@@ -1,4 +0,0 @@
-from itertools import chain,combinations
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        return list(chain.from_iterable(combinations(nums,i) for i in range(len(nums) + 1)))
